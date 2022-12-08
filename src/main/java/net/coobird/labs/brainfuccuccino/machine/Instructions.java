429d8b2f30231f96d9040d01d1289e71d3d82fd7
Chris Kroells <coobird@coobird.net>
2022-12-09 02:25:03 +0900

    Replace map-based lookup with switch to improve performance.

--- src/main/java/net/coobird/labs/brainfuccuccino/machine/Instructions.java
+++ src/main/java/net/coobird/labs/brainfuccuccino/machine/Instructions.java
@@ -1,38 +1,36 @@
 package net.coobird.labs.brainfuccuccino.machine;
 
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
-
 public enum Instructions {
-    INCREMENT_POINTER('>'),
-    DECREMENT_POINTER('<'),
-    INCREMENT_VALUE('+'),
-    DECREMENT_VALUE('-'),
-    OUTPUT_VALUE('.'),
-    INPUT_VALUE(','),
-    BEGIN_LOOP('['),
-    END_LOOP(']'),
-    NOP('\0');
-
-    private final byte value;
-
-    Instructions(char character) {
-        this.value = (byte) character;
-    }
-
-    private final static Map<Byte, Instructions> INSTRUCTION_MAPPING;
-
-    static {
-        Map<Byte, Instructions> mapping = new HashMap<>();
-        for (Instructions instruction : Instructions.values()) {
-            mapping.put(instruction.value, instruction);
-        }
-        INSTRUCTION_MAPPING = Collections.unmodifiableMap(mapping);
-    }
+    INCREMENT_POINTER,
+    DECREMENT_POINTER,
+    INCREMENT_VALUE,
+    DECREMENT_VALUE,
+    OUTPUT_VALUE,
+    INPUT_VALUE,
+    BEGIN_LOOP,
+    END_LOOP,
+    NOP;
 
     public static Instructions getInstruction(byte value) {
-        Instructions instruction = INSTRUCTION_MAPPING.get(value);
-        return instruction != null ? instruction : NOP;
+        switch (value) {
+            case '>':
+                return INCREMENT_POINTER;
+            case '<':
+                return DECREMENT_POINTER;
+            case '+':
+                return INCREMENT_VALUE;
+            case '-':
+                return DECREMENT_VALUE;
+            case '.':
+                return OUTPUT_VALUE;
+            case ',':
+                return INPUT_VALUE;
+            case '[':
+                return BEGIN_LOOP;
+            case ']':
+                return END_LOOP;
+            default:
+                return NOP;
+        }
     }
 }
