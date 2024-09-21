6fb7b17f13fd1e1a2923f3044dce9609e1daa170
Chris Kroells <coobird@coobird.net>
2024-09-21 23:57:04 +0900

    Add GUI-based monitoring of execution.

--- src/test/java/net/coobird/labs/brainfuccuccino/machine/SignedByteBoundedBrainfuckMachineTest.java
+++ src/test/java/net/coobird/labs/brainfuccuccino/machine/SignedByteBoundedBrainfuckMachineTest.java
@@ -26,12 +26,22 @@
 
 package net.coobird.labs.brainfuccuccino.machine;
 
+import net.coobird.labs.brainfuccuccino.Utils;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.DisabledIfSystemProperty;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import javax.swing.JFrame;
+import javax.swing.JTextArea;
+import javax.swing.SwingUtilities;
+import java.awt.BorderLayout;
+import java.awt.Font;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicLong;
 
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -71,5 +81,103 @@ public class SignedByteBoundedBrainfuckMachineTest {
         assertEquals(8, state.getProgramCounter());
         assertEquals(2, state.getDataPointer());
         assertArrayEquals(new Byte[] {1, 2, 3}, state.getMemory());
+    }
+
+    @DisabledIfSystemProperty(named = "java.awt.headless", matches = "true")
+    @Test
+    public void inspectionByGUI() throws Exception {
+        final JFrame frame = new JFrame();
+        final JTextArea textArea = new JTextArea();
+        textArea.setFont(new Font("Monospaced", Font.PLAIN, 12));
+        textArea.setLineWrap(true);
+        frame.setLayout(new BorderLayout());
+        frame.add(textArea, BorderLayout.CENTER);
+        frame.setSize(400, 300);
+        frame.setLocationByPlatform(true);
+        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        frame.setVisible(true);
+
+        final SignedByteBoundedBrainfuckMachine machine = new SignedByteBoundedBrainfuckMachine(10);
+
+        AtomicBoolean isRunning = new AtomicBoolean(true);
+        new Thread(() -> {
+            long startTime = System.currentTimeMillis();
+            long lastCall = 0;
+            long lastInstructionsExecuted = 0;
+            long lastProgramCounterChanges = 0;
+            AtomicLong maxInstructionsPerSec = new AtomicLong(0);
+            AtomicLong maxProgramCounterChangesPerSec = new AtomicLong(0);
+
+            while (isRunning.get()) {
+                long now = System.currentTimeMillis();
+                if (now - lastCall > 50) {
+                    final Introspectable.MachineState<Byte> state = machine.getState();
+                    final ExecutionStatistics statistics = machine.getStatistics();
+                    final double elapsedTime = (double)(now - lastCall);
+
+                    /*
+                     * These rates will be inaccurate for running programs that
+                     * execution within 50 milliseconds.
+                     */
+                    double instructionsPerSec =
+                            (statistics.getInstructionsExecuted() - lastInstructionsExecuted) * (1000 / elapsedTime);
+                    double programCounterChangesPerSec =
+                            (statistics.getProgramCounterChanges() - lastProgramCounterChanges) * (1000 / elapsedTime);
+
+                    // Store the double as bits in the integer, as AtomicDouble is not provided.
+                    maxInstructionsPerSec.set(
+                            Double.doubleToLongBits(
+                                    Math.max(
+                                            Double.longBitsToDouble(maxInstructionsPerSec.get()),
+                                            instructionsPerSec
+                                    )
+                            )
+                    );
+                    maxProgramCounterChangesPerSec.set(
+                            Double.doubleToLongBits(
+                                    Math.max(
+                                            Double.longBitsToDouble(maxProgramCounterChangesPerSec.get()),
+                                            programCounterChangesPerSec
+                                    )
+                            )
+                    );
+
+                    SwingUtilities.invokeLater(() -> {
+                        String s = String.format("currentTimestamp: %s%n", now);
+                        s += String.format("instructionsExecuted: %,d%n", statistics.getInstructionsExecuted());
+                        s += String.format("instructionsSkipped: %,d%n", statistics.getInstructionsSkipped());
+                        s += String.format("programCounterChanges: %,d%n", statistics.getProgramCounterChanges());
+                        s += String.format("memory: %s%n", Arrays.toString(state.getMemory()));
+                        s += "\n";
+                        s += String.format("instructionsPerSec: %,.1f%n", instructionsPerSec);
+                        s += String.format("programCounterChangesPerSec: %,.1f%n", programCounterChangesPerSec);
+                        s += String.format(
+                                "max(instructionsPerSec): %,.1f%n",
+                                Double.longBitsToDouble(maxInstructionsPerSec.get())
+                        );
+                        s += String.format(
+                                "max(programCounterChangesPerSec): %,.1f%n",
+                                Double.longBitsToDouble(maxProgramCounterChangesPerSec.get())
+                        );
+                        s += String.format("elapsedTime: %,.3f sec%n", (now - startTime) / 1000.0);
+                        textArea.setText(s);
+                    });
+                    lastInstructionsExecuted = statistics.getInstructionsExecuted();
+                    lastProgramCounterChanges = statistics.getProgramCounterChanges();
+                    lastCall = now;
+                }
+                try {
+                    Thread.sleep(10);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }).start();
+
+        machine.evaluate(Utils.getScriptFromResources("dots.bf").getBytes(), null, System.out);
+
+        // Just to enable seeing the JFrame
+        Thread.sleep(5000);
+        isRunning.set(false);
     }
 }
