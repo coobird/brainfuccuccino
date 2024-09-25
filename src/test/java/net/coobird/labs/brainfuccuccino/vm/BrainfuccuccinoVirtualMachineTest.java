1dfb8b952e23af3e5ac8943fd60b13bcea50e3ce
Chris Kroells <coobird@coobird.net>
2024-09-26 02:09:59 +0900

    Expand to single class imports.

--- src/test/java/net/coobird/labs/brainfuccuccino/vm/BrainfuccuccinoVirtualMachineTest.java
+++ src/test/java/net/coobird/labs/brainfuccuccino/vm/BrainfuccuccinoVirtualMachineTest.java
@@ -32,7 +32,8 @@ import net.coobird.labs.brainfuccuccino.machine.state.MachineState;
 import net.coobird.labs.brainfuccuccino.vm.model.Instruction;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.*;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
